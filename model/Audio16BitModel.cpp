ee91c3e7400d7bda34eb6f00799ef7ce4d303028
Zoltán Gotthardt <gotty@freemail.hu>
2026-02-26 23:34:26 +0100

    Cosmetic in AudioModel16: round, not roundf.

--- model/Audio16BitModel.cpp
+++ model/Audio16BitModel.cpp
@@ -41,138 +41,139 @@ void Audio16BitModel::setParam(int info) {
   }
 }
 
-void Audio16BitModel::mix(Mixer &m) {
+void Audio16BitModel::mix(Mixer& m) {
   INJECT_SHARED_bpos
-  INJECT_SHARED_blockPos
-  if( bpos == 0 && blockPos != 0 ) {
-    ch = (stereo) != 0 ? (blockPos & 2) >> 1 : 0;
-    lsb = (blockPos & 1) ^ static_cast<uint32_t>(wMode < 4);
-    if((blockPos & 1) == 0 ) {
-      sample = (wMode < 4) ? s2(2) : t2(2);
-      const int pCh = ch ^stereo;
-      int i = 0;
-      for( errLog = 0; i < nOLS; i++ ) {
-        ols[i][pCh]->update(sample);
-        residuals[i][pCh] = sample - prd[i][pCh][0];
-        const uint32_t absResidual = static_cast<uint32_t>(abs(residuals[i][pCh]));
-        mask += mask + static_cast<uint32_t>(absResidual > 128);
-        errLog += square(absResidual >> 6);
-      }
-      for( int j = 0; j < nLMS; j++ ) {
-        lms[j][pCh].get()->update(sample);
-      }
-      for( ; i < nSSM; i++ ) {
-        residuals[i][pCh] = sample - prd[i][pCh][0];
-      }
-      errLog = min(0xF, ilog2(errLog));
-
-      if( stereo != 0 ) {
-        for( int i = 1; i <= 24; i++ ) {
-          ols[0][ch]->add((double)x2(i));
+    INJECT_SHARED_blockPos
+    if (bpos == 0 && blockPos != 0) {
+      ch = (stereo) != 0 ? (blockPos & 2) >> 1 : 0;
+      lsb = (blockPos & 1) ^ static_cast<uint32_t>(wMode < 4);
+      if ((blockPos & 1) == 0) {
+        sample = (wMode < 4) ? s2(2) : t2(2);
+        const int pCh = ch ^ stereo;
+        int i = 0;
+        for (errLog = 0; i < nOLS; i++) {
+          ols[i][pCh]->update(sample);
+          residuals[i][pCh] = sample - prd[i][pCh][0];
+          const uint32_t absResidual = static_cast<uint32_t>(abs(residuals[i][pCh]));
+          mask += mask + static_cast<uint32_t>(absResidual > 128);
+          errLog += square(absResidual >> 6);
         }
-        for( int i = 1; i <= 104; i++ ) {
-          ols[0][ch]->add((double)x1(i));
+        for (int j = 0; j < nLMS; j++) {
+          lms[j][pCh].get()->update(sample);
         }
-      } else {
-        for( int i = 1; i <= 128; i++ ) {
-          ols[0][ch]->add((double)x1(i));
+        for (; i < nSSM; i++) {
+          residuals[i][pCh] = sample - prd[i][pCh][0];
+        }
+        errLog = min(0xF, ilog2(errLog));
+
+        if (stereo != 0) {
+          for (int i = 1; i <= 24; i++) {
+            ols[0][ch]->add((double)x2(i));
+          }
+          for (int i = 1; i <= 104; i++) {
+            ols[0][ch]->add((double)x1(i));
+          }
+        }
+        else {
+          for (int i = 1; i <= 128; i++) {
+            ols[0][ch]->add((double)x1(i));
+          }
+        }
+
+        int k1 = 90;
+        int k2 = k1 - 12 * stereo;
+        for (int j = (i = 1); j <= k1; j++, i += 1
+          << (static_cast<int>(j > 16) +
+            static_cast<int>(j > 32) +
+            static_cast<int>(j > 64))) {
+          ols[1][ch]->add((double)x1(i));
+        }
+        for (int j = (i = 1); j <= k2; j++, i += 1
+          << (static_cast<int>(j > 5) +
+            static_cast<int>(j > 10) +
+            static_cast<int>(j > 17) +
+            static_cast<int>(j > 26) +
+            static_cast<int>(j > 37))) {
+          ols[2][ch]->add((double)x1(i));
+        }
+        for (int j = (i = 1); j <= k2; j++, i += 1
+          << (static_cast<int>(j > 3) +
+            static_cast<int>(j > 7) +
+            static_cast<int>(j > 14) +
+            static_cast<int>(j > 20) +
+            static_cast<int>(j > 33) +
+            static_cast<int>(j > 49))) {
+          ols[3][ch]->add((double)x1(i));
+        }
+        for (int j = (i = 1); j <= k2; j++, i += 1 +
+          static_cast<int>(j > 4) +
+          static_cast<int>(j > 8)) {
+          ols[4][ch]->add((double)x1(i));
+        }
+        for (int j = (i = 1); j <= k1; j++, i += 2 +
+          (static_cast<int>(j > 3) +
+            static_cast<int>(j > 9) +
+            static_cast<int>(j > 19) +
+            static_cast<int>(j > 36) +
+            static_cast<int>(j > 61))) {
+          ols[5][ch]->add((double)x1(i));
+        }
+
+        if (stereo != 0) {
+          for (i = 1; i <= k1 - k2; i++) {
+            const double s = (double)x2(i);
+            ols[2][ch]->add(s);
+            ols[3][ch]->add(s);
+            ols[4][ch]->add(s);
+          }
+        }
+
+        k1 = 28, k2 = k1 - 6 * stereo;
+        for (i = 1; i <= k2; i++) {
+          ols[6][ch]->add((double)x1(i));
+        }
+        for (i = 1; i <= k1 - k2; i++) {
+          ols[6][ch]->add((double)x2(i));
+        }
+
+        k1 = 32, k2 = k1 - 8 * stereo;
+        for (i = 1; i <= k2; i++) {
+          ols[7][ch]->add((double)x1(i));
+        }
+        for (i = 1; i <= k1 - k2; i++) {
+          ols[7][ch]->add((double)x2(i));
+        }
+
+        for (i = 0; i < nOLS; i++) {
+          double prediction = ols[i][ch]->predict();
+          prd[i][ch][0] = signedClip16(static_cast<int>(round(prediction)));
+        }
+        for (; i < nOLS + nLMS; i++) {
+          float prediction = lms[i - nOLS][ch]->predict(sample);
+          prd[i][ch][0] = signedClip16(static_cast<int>(round(prediction)));
+        }
+        prd[i++][ch][0] = signedClip16(x1(1) * 2 - x1(2));
+        prd[i++][ch][0] = signedClip16(x1(1) * 3 - x1(2) * 3 + x1(3));
+        prd[i][ch][0] = signedClip16(x1(1) * 4 - x1(2) * 6 + x1(3) * 4 - x1(4));
+        for (i = 0; i < nSSM; i++) {
+          prd[i][ch][1] = signedClip16(prd[i][ch][0] + residuals[i][pCh]);
         }
       }
-
-      int k1 = 90;
-      int k2 = k1 - 12 * stereo;
-      for( int j = (i = 1); j <= k1; j++, i += 1 
-              << (static_cast<int>(j > 16) + 
-                  static_cast<int>(j > 32) + 
-                  static_cast<int>(j > 64))) {
-        ols[1][ch]->add((double)x1(i));
-      }
-      for( int j = (i = 1); j <= k2; j++, i += 1
-              << (static_cast<int>(j > 5) + 
-                  static_cast<int>(j > 10) + 
-                  static_cast<int>(j > 17) + 
-                  static_cast<int>(j > 26) +
-                  static_cast<int>(j > 37))) {
-        ols[2][ch]->add((double)x1(i));
-      }
-      for( int j = (i = 1); j <= k2; j++, i += 1
-              << (static_cast<int>(j > 3) + 
-                  static_cast<int>(j > 7) + 
-                  static_cast<int>(j > 14) + 
-                  static_cast<int>(j > 20) +
-                  static_cast<int>(j > 33) + 
-                  static_cast<int>(j > 49))) {
-        ols[3][ch]->add((double)x1(i));
-      }
-      for( int j = (i = 1); j <= k2; j++, i += 1 + 
-                  static_cast<int>(j > 4) + 
-                  static_cast<int>(j > 8)) {
-        ols[4][ch]->add((double)x1(i));
-      }
-      for( int j = (i = 1); j <= k1; j++, i += 2 + 
-                  (static_cast<int>(j > 3) + 
-                   static_cast<int>(j > 9) + 
-                   static_cast<int>(j > 19) +
-                   static_cast<int>(j > 36) + 
-                   static_cast<int>(j > 61))) {
-        ols[5][ch]->add((double)x1(i));
-      }
-
-      if( stereo != 0 ) {
-        for( i = 1; i <= k1 - k2; i++ ) {
-          const double s = (double)x2(i);
-          ols[2][ch]->add(s);
-          ols[3][ch]->add(s);
-          ols[4][ch]->add(s);
-        }
-      }
-
-      k1 = 28, k2 = k1 - 6 * stereo;
-      for( i = 1; i <= k2; i++ ) {
-        ols[6][ch]->add((double)x1(i));
-      }
-      for( i = 1; i <= k1 - k2; i++ ) {
-        ols[6][ch]->add((double)x2(i));
-      }
-
-      k1 = 32, k2 = k1 - 8 * stereo;
-      for( i = 1; i <= k2; i++ ) {
-        ols[7][ch]->add((double)x1(i));
-      }
-      for( i = 1; i <= k1 - k2; i++ ) {
-        ols[7][ch]->add((double)x2(i));
-      }
-
-      for( i = 0; i < nOLS; i++ ) {
-        double prediction = ols[i][ch]->predict();
-        prd[i][ch][0] = signedClip16(static_cast<int>(roundf(prediction)));
-      }
-      for( ; i < nOLS + nLMS; i++ ) {
-        float prediction = lms[i - nOLS][ch]->predict(sample);
-        prd[i][ch][0] = signedClip16(static_cast<int>(roundf(prediction)));
-      }
-      prd[i++][ch][0] = signedClip16(x1(1) * 2 - x1(2));
-      prd[i++][ch][0] = signedClip16(x1(1) * 3 - x1(2) * 3 + x1(3));
-      prd[i][ch][0] = signedClip16(x1(1) * 4 - x1(2) * 6 + x1(3) * 4 - x1(4));
-      for( i = 0; i < nSSM; i++ ) {
-        prd[i][ch][1] = signedClip16(prd[i][ch][0] + residuals[i][pCh]);
-      }
+      shared->State.Audio = 0x80 | (mxCtx = ilog2(min(0x1F, bitCount(mask))) * 4 + ch * 2 + lsb);
     }
-    shared->State.Audio = 0x80 | (mxCtx = ilog2(min(0x1F, bitCount(mask))) * 4 + ch * 2 + lsb);
-  }
 
   INJECT_SHARED_c0
-  INJECT_SHARED_c1
-  const short b = short((wMode < 4) ?
-                         (lsb) != 0 ? 
-                            uint8_t(c0 << (8 - bpos)) :
-                            (c0 << (16 - bpos)) | c1
-                       :
-                         (lsb) != 0 ?
-                            (c1 << 8) | uint8_t(c0 << (8 - bpos)) :
-                            c0 << (16 - bpos));
+    INJECT_SHARED_c1
+    const short b = short((wMode < 4) ?
+      (lsb) != 0 ?
+      uint8_t(c0 << (8 - bpos)) :
+      (c0 << (16 - bpos)) | c1
+      :
+      (lsb) != 0 ?
+      (c1 << 8) | uint8_t(c0 << (8 - bpos)) :
+      c0 << (16 - bpos));
 
-  for( int i = 0; i < nSSM; i++ ) {
+  for (int i = 0; i < nSSM; i++) {
     const uint32_t ctx0 = uint16_t(prd[i][ch][0] - b);
     const uint32_t ctx1 = uint16_t(prd[i][ch][1] - b);
 
